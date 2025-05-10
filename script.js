f1a9722fcc9e88f4ad82206d373bee6d5ef7425f
Code_H29/ <viniccamarg13@gmail.com>
2025-05-09 23:13:44 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const hours   = document.getElementById("hours")
+const minutes = document.getElementById("minutes")
+const seconds = document.getElementById("seconds")
+
+let interval = setInterval(() => {
+    var time = new Date()
+
+    var HH = String(time.getHours())  .padStart(2, "0")
+    var mm = String(time.getMinutes()).padStart(2, "0")
+    var ss = String(time.getSeconds()).padStart(2, "0")
+
+    hours.innerText   = HH
+    minutes.innerText = mm
+    seconds.innerText = ss
+})
